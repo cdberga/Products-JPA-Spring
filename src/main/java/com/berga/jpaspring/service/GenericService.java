ac48ea50ec4b019819f926ce3f19691fa705bbcd
Carlos <carlos.bergamasco@gmail.com>
2020-08-18 12:01:05 -0300

    Add Generic Service

---
+++ src/main/java/com/berga/jpaspring/service/GenericService.java
@@ -0,0 +1,43 @@
+package com.berga.jpaspring.service;
+
+import java.io.Serializable;
+
+import org.hibernate.service.spi.ServiceException;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GenericService<T, ID extends Serializable> {
+
+	public default Iterable<T> findAll(){
+		return getRepository().findAll();
+	}
+	
+	public default T get(ID id) {
+		return (T) getRepository().findById(id);
+	}
+	
+	public default T save(T entity) {
+		return getRepository().save(entity);
+	}
+	
+	public default void delete(ID id) {
+		if(getRepository().existsById(id)) {
+			getRepository().deleteById(id);
+		}
+		else {
+			throw new ServiceException("Object id does not exist: " + id);
+		}
+	}
+	
+	public default void update(T entity) {
+		if(getRepository().existsById(getId(entity))) {
+			getRepository().save(entity);
+		}
+		else {
+			throw new ServiceException("Can not update beacause object does not exist in DB: " + entity);
+		}
+	}
+	
+	public ID getId(T entity);
+	
+	public CrudRepository<T, ID> getRepository();
+}
