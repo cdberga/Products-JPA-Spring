62ddf3bcf6cdbf86e70cfb46326b3214acd3631e
Carlos <carlos.bergamasco@gmail.com>
2020-08-18 15:10:51 -0300

    Add UserController

---
+++ src/main/java/com/berga/jpaspring/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.berga.jpaspring.controller;
+
+import java.net.URI;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.berga.jpaspring.model.User;
+import com.berga.jpaspring.service.UserService;
+import com.berga.jpaspring.vo.ResponseVO;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	private final UserService userService;
+	
+	public UserController(final UserService userService) {
+		this.userService = userService;
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ResponseVO<Iterable<User>>> findAll() {
+		return ResponseEntity.ok(new ResponseVO<>(userService.findAll()));
+	}
+	
+	@GetMapping(value="/{user_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ResponseVO<User>> get(@PathVariable("user_id") String userId) {
+		return ResponseEntity.ok(new ResponseVO<>(userService.get(userId)));
+	}
+	
+	@PostMapping(consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ResponseVO<User>> createUser(@RequestBody User user) {
+		User savedUser = userService.save(user);
+		return ResponseEntity.created(URI.create("/" + savedUser.getUserId())).body(new ResponseVO<>(savedUser));
+	}
+	
+	@PutMapping(consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> updateUser(@RequestBody User user) {
+		userService.update(user);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value="/{user_id}")
+	public ResponseEntity<ResponseVO<String>> delete(@PathVariable("user_id") String userId) {
+		userService.delete(userId);
+		return ResponseEntity.ok(new ResponseVO<>(userId));
+	}
+}
